b4f5ca9e176645ebb0c7a808e7b936b5d90efe08
JunYeong <skystar0071@naver.com>
2024-07-03 12:38:10 +0900

    day5

--- samples/fake/fake/src/data/util.ts
+++ samples/fake/fake/src/data/util.ts
@@ -25,12 +25,11 @@ export const makeArray5 = (length: number) => new Array(length).fill(null);
 */
 
 //[length만큼 null(undefined)로 채워짐]
-export const makeArray = (length: number) => new Array(null);
+export const makeArray = (length: number) => new Array(length).fill(null);
 
 //[length = max - min 만큼 null로 채워진 배열에 map으로 값 채워넣기.]
 export const range = (min: number, max: number): number[] =>
   makeArray(max - min).map((notUsed, index) => index + min);
-
 
 export const random = (min: number, max: number): number =>
   Math.floor(Math.random() * (max - min)) + min;
--- samples/fake/fake/src/index.tsx
+++ samples/fake/fake/src/index.tsx
@@ -8,6 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 // const children = ["강", "준", "영"].map((n: string) => (
 //   <h3>Hello World! {n}</h3>
 // ));
+
 const children = D.makeArray(10).map((notUsed, index) => (
   <div key={index}>
     <p>{D.randomID()}</p>
@@ -17,14 +18,10 @@ const children = D.makeArray(10).map((notUsed, index) => (
     <img src={D.randomAvatar()} width={100} height={100} />
   </div>
 ));
+
 const rootVirtualDOM = <div>{children}</div>;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(rootVirtualDOM);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
