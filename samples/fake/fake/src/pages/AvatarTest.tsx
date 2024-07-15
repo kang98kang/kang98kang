5437df681f4be63b605364b7a5e7bf78e275d4e4
JunYeong <skystar0071@naver.com>
2024-07-15 16:53:16 +0900

    715

--- samples/fake/fake/src/pages/AvatarTest.tsx
+++ samples/fake/fake/src/pages/AvatarTest.tsx
@@ -1,20 +1,20 @@
 import { Div, Title, Avatar } from "../components";
 import * as D from "../data";
 
-export default function AvatarTest() {
-  const avatars = D.range(0, 10).map((index) => (
-    <Avatar
-      className="inline-block -ml-6 border-white"
-      key={index}
-      src={D.randomAvatar()}
-    ></Avatar>
-  ));
-  return (
-    <section className=" mt-4 ">
-      <Title>AvatarTest</Title>
-      <div className="display-flex justify-between px-12 py-4 m-8 bg-blue-300">
-        {avatars}
-      </div>
-    </section>
-  );
-}
+// export default function AvatarTest() {
+//   const avatars = () => (
+//     <Avatar
+//       className="inline-block -ml-6 border-white"
+
+//       src={"./"}
+//     ></Avatar>
+//   );
+//   return (
+//     <section className=" mt-4 ">
+//       <Title>AvatarTest</Title>
+//       <div className="display-flex justify-between px-12 py-4 m-8 bg-blue-300">
+//         {avatars}
+//       </div>
+//     </section>
+//   );
+// }
