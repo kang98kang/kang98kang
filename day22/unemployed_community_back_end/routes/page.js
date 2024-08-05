4271e60407e6e28ab8731ed8249b5da5db4b0945
JunYeong <skystar0071@naver.com>
2024-08-05 12:05:40 +0900

    0805

---
+++ day22/unemployed_community_back_end/routes/page.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const { renderMain, renderHashtag } = require("../controllers/page");
+
+router.use((req, res, next) => {
+  res.locals.user = req.user;
+  res.locals.followerCount = req.user ? req.user.Followers.length : 0;
+  res.locals.followingCount = req.user ? req.user.Followers.length : 0;
+  res.locals.followingIdList = req.user
+    ? req.user.Followings.map((f) => f.id)
+    : [];
+  next();
+});
+
+router.get("/", renderMain);
+
+router.get("/hashtag", renderHashtag);
+
+module.exports = router;
